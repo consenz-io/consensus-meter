79ba13f0b326a29250faf0858b4ed1036d803b1f
Aviran Katz <aviran.katz@gmail.com>
2022-09-12 13:34:30 +0300

    feat: iterations

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import './App.css';
-import { Alert, AlertTitle, Box, Card, CardContent, CardHeader, Container, createTheme, IconButton, Stack, SvgIcon, ThemeProvider, Typography } from '@mui/material';
+import { Alert, AlertTitle, Box, Button, Card, CardContent, CardHeader, Container, createTheme, IconButton, Stack, SvgIcon, ThemeProvider, Typography } from '@mui/material';
 import {ReactComponent as UpArrow} from './svg/up.svg'
 import {ReactComponent as DownArrow} from './svg/down.svg'
 import {ReactComponent as LeftChevron} from './svg/left.svg'
 import {ReactComponent as RightChevron} from './svg/right.svg'
+import { calculateDocumentConsensus, calculateSectionConsensus } from './calculator';
+
+interface Iteration {
+  upvotes: number;
+  downvotes: number;
+  users: number;
+  consensus: number;
+  newThreshold: number;
+}
+
+function getRandomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function pluralize(content: string) {
+  return content + (content.includes('1') ? '' : 's');
+}
 
 function App() {
   const [documentUsers, setDocumentUsers] = React.useState(1);
   const [consensus, setConsensus] = React.useState(1);
-  const [sections, setSections] = React.useState([{suggestions: [{upvotes: 1, downvotes: 0}], currentSuggestion: 0}]);
+  const [iterations, setIterations] = React.useState<Iteration[]>([{upvotes: 1, downvotes: 0, users: 1, consensus: 1, newThreshold: 1}]);
+  function addIteration():void {
+    const newUsers = getRandomNumber(1, 10);
+    const lastIteration = iterations[iterations.length - 1];
+    const upvotes = getRandomNumber(lastIteration.newThreshold, lastIteration.newThreshold + newUsers);
+    const downvotes = Math.floor(Math.random() * (newUsers - upvotes));
+    const users = lastIteration.users + newUsers;
+    const consensus = calculateSectionConsensus(upvotes, downvotes, users, 'approval');
+    const newThreshold = Math.ceil(consensus * users);
+    setIterations([...iterations, {upvotes, downvotes, users, consensus,newThreshold }]);
+    setDocumentUsers(users);
+    setConsensus(calculateDocumentConsensus([...iterations.map(i => i.consensus), consensus]));
+  }
   return (
     <ThemeProvider theme={createTheme({
       typography: {
@@ -29,34 +58,25 @@ function App() {
     <Stack alignContent="center" justifyContent="center" spacing={4}>
       <Alert severity="info">
         <AlertTitle>Summary</AlertTitle>
+          <Typography>Total Iterations: {iterations.length}</Typography>
           <Typography>Document Users: {documentUsers}</Typography>
           <Typography>Document Consensus: {consensus}</Typography>
         </Alert>
-        <Stack spacing={2} paddingX={2}>
-          {sections.map((section, index) => (
-          <Card key={index} variant="outlined">
-            <CardHeader title={`Section ${index + 1}`} />
+        <Stack spacing={2}>
+          {iterations.map((iteration, i) => (
+          <Card key={i} variant="outlined">
+            <CardHeader title={`Iteration ${i + 1}`} />
             <CardContent>
-              <Stack direction="row" alignItems="center" spacing={2}>
-                <IconButton size="large" color="warning">
-                  <SvgIcon><LeftChevron/></SvgIcon>
-                </IconButton>
-                <Stack>
-                  <IconButton color="success"><SvgIcon><UpArrow/></SvgIcon></IconButton>
-                  <IconButton color="error"><SvgIcon><DownArrow/></SvgIcon></IconButton>
-                </Stack>
-                <Box flexGrow={1}>
-                <Typography variant="h6">Suggestion {section.currentSuggestion + 1}</Typography>
-                <Typography variant="subtitle1" color="success.main">{section.suggestions[section.currentSuggestion].upvotes} upvotes</Typography>
-                <Typography variant="subtitle1" color="error.main">{section.suggestions[section.currentSuggestion].downvotes} downvotes</Typography>
-                </Box>
-                <IconButton size="large" color="warning">
-                  <SvgIcon><RightChevron/></SvgIcon>
-                </IconButton>
-              </Stack>
+              <Typography variant="subtitle1">Users: {iteration.users}</Typography>
+              <Typography variant="subtitle1" color="success.main">{iteration.upvotes} upvotes</Typography>
+              <Typography variant="subtitle1" color="error.main">{iteration.downvotes} downvotes</Typography>
+              <Typography variant="subtitle1" color="primary.main">Consensus: {iteration.consensus}</Typography>
             </CardContent>
           </Card>
           ))}
+          <Box position="fixed" bottom={16}>
+            <Button size="large" variant="contained" onClick={addIteration}>Add Iteration</Button>
+          </Box>
         </Stack>
     </Stack>
     </Container>
