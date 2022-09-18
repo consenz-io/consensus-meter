6f3626e5bf8d4aa18fb10a9b280de9ef6bf79b77
Aviran Katz <aviran.katz@gmail.com>
2022-09-18 10:30:38 +0300

    feat: chart

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Alert, AlertTitle, Box, Button, Card, CardContent, CardHeader, Container, createTheme, Dialog, DialogContent, IconButton, Stack, styled, SvgIcon, TextField, ThemeProvider, Typography } from '@mui/material';
+import { Alert, AlertTitle, Button, Card, CardContent, CardHeader, Container, createTheme, Dialog, DialogContent, IconButton, Stack, styled, SvgIcon, TextField, ThemeProvider, Typography } from '@mui/material';
 import { calculateDocumentConsensus, calculateSectionConsensus } from './calculator';
 import {ReactComponent as Cog} from './svg/cog.svg'
+import {ReactComponent as Chart} from './svg/chart.svg'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import {Line} from 'react-chartjs-2'
 
 interface Iteration {
   upvotes: number;
@@ -10,6 +22,7 @@ interface Iteration {
   users: number;
   consensus: number;
   newThreshold: number;
+  documentConsensus: number;
 }
 
 function getRandomInt(min: number, max: number): number {
@@ -23,13 +36,24 @@ const RotatingIcon = styled(IconButton)({
   'transition': '0.5s',
 });
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 function App() {
+  const [isChartVisible, setIsChartVisible] = React.useState(false);
   const [documentUsers, setDocumentUsers] = React.useState(1);
   const [consensus, setConsensus] = React.useState(1);
-  const [iterations, setIterations] = React.useState<Iteration[]>([{upvotes: 1, downvotes: 0, users: 1, consensus: 1, newThreshold: 1}]);
+  const [iterations, setIterations] = React.useState<Iteration[]>([{upvotes: 1, downvotes: 0, users: 1, consensus: 1, newThreshold: 1, documentConsensus: 1}]);
   const [settings, setSettings] = React.useState(false);
   const [newUsersLimit, setNewUsersLimit] = React.useState(10);
-  
+
   function addIteration(): void {
     const lastIteration = iterations[iterations.length - 1];
     const newUsers = getRandomInt(0, newUsersLimit);
@@ -39,7 +63,7 @@ function App() {
     const sectionConsensus = calculateSectionConsensus(upvotes, downvotes, users, 'approval');
     const documentConsensus = calculateDocumentConsensus([...iterations.map(i => i.consensus), sectionConsensus])
     const newThreshold = Math.ceil(documentConsensus * users);
-    setIterations([...iterations, {upvotes, downvotes, users, consensus: sectionConsensus,newThreshold }]);
+    setIterations([...iterations, {upvotes, downvotes, users, consensus: sectionConsensus,newThreshold, documentConsensus }]);
     setDocumentUsers(users);
     setConsensus(calculateDocumentConsensus([...iterations.map(i => i.consensus), sectionConsensus]));
   }
@@ -59,10 +83,21 @@ function App() {
         borderRadius: 16
       }
     })}>
-    <RotatingIcon sx={{position: 'absolute', margin: 1}} onClick={() => setSettings(true)}><SvgIcon><Cog/></SvgIcon></RotatingIcon>
+    <Stack position="absolute" margin={1} spacing={1}>
+      <IconButton onClick={() => setIsChartVisible(true)}><SvgIcon><Chart/></SvgIcon></IconButton>
+      <RotatingIcon onClick={() => setSettings(true)}><SvgIcon><Cog/></SvgIcon></RotatingIcon>
+    </Stack>
     <Dialog open={settings} onClose={() => setSettings(false)}>
       <DialogContent>
         <TextField label="New users per iteration limit" type="number" value={newUsersLimit} onChange={(v) => setNewUsersLimit(+v.target.value)}/>
+      </DialogContent>
+    </Dialog>
+    <Dialog open={isChartVisible} onClose={() => setIsChartVisible(false)} maxWidth="md" fullWidth>
+      <DialogContent>
+        <Line data={{labels:iterations.map((o, i) => i), datasets:[
+            {data:iterations.map(i => i.documentConsensus), label: "DocumentConsensus", borderColor: "hsl(100, 50%, 50%)"},
+            {data:iterations.map(i => i.consensus),label:"Section Consensus", borderColor: "hsl(200, 50%, 50%)"},
+          ]}} />
       </DialogContent>
     </Dialog>
     <Container sx={{paddingY: 8,height:'100vh'}}>
@@ -86,9 +121,9 @@ function App() {
             </CardContent>
           </Card>
           ))}
-          <Box position="fixed" bottom={16}>
-            <Button size="large" variant="contained" onClick={addIteration}>Add Iteration</Button>
-          </Box>
+        </Stack>
+        <Stack position="fixed" bottom={16} display="flex" justifyItems="end" flexGrow={1}>
+          <Button size="large" variant="contained" onClick={addIteration}>Add Iteration</Button>
         </Stack>
     </Stack>
     </Container>
